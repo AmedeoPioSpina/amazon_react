71e737247fb640b3516df5ecb7ef6e896ac3a662
RoninMirrorImage <spina.amedeo.pio@gmail.com>
2023-04-11 17:29:55 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+const App = () => {
+  return (
+      <div className="app">
+        <nav>
+
+          <div className="side-menu">
+            <button>
+              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAYUlEQVR4nO3WyQnAMAwFUZcXp/+zDMF9TDrQJcYLmdfBRwimFEmSBgFuoLG/BtRsyMM5+i+GVCDYXwDX51+SJEmaCDN+iZ5dxIyfLMx4SZJ0Gsz4JXp2ETN+sjDjJUlloBfbFyzbu4GNjwAAAABJRU5ErkJggg=="/>
+            </button>
+            <div className="">
+
+            </div>
+          </div>
+
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+
+          <div className="log-in">
+
+          </div>
+
+          <div className="cart">
+
+          </div>
+
+          <div className="search-bar">
+
+          </div>
+
+          //dare un nome alla classe
+          <ul className="">
+            <li>Deals</li>
+            <li>Amazon Basic</li>
+            <li>Best Sellers</li>
+            <li>Video</li>
+            <li>Livestreams</li>
+            <li>Books</li>
+            <li>New Releases</li>
+            <li>Home</li>
+            <li>PC</li>
+            <li>Health & Household</li>
+            <li>Gift Cards</li>
+            <li>Music</li>
+            <li>Lists</li>
+          </ul>
+
+        </nav>
+      </div>
+  );
+}
+
+export default App;
